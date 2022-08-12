8332551918db151b90f1f1054c69408beb88aa79
Muhammet Ali KOÇ <71590181+malikoc2020@users.noreply.github.com>
2022-08-12 13:59:49 +0300

    EFCore.Repository tier added.

---
+++ EFCore.Repository/Abstracts/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using EFCore.Context;
+
+namespace EntityFramework.Repository.Abstracts
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        AppDbContext Context { get; }
+        void Commit();
+    }
+}
---
+++ EFCore.Repository/Concretes/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using EFCore.Context;
+using EntityFramework.Repository.Abstracts;
+
+namespace EntityFramework.Repository.Concretes
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public AppDbContext Context { get; }
+        public UnitOfWork(AppDbContext context)
+        {
+            Context = context;
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            Context?.Dispose();
+        }
+    }
+}
