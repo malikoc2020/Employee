1caaa1256e25dcb5404754b669fd751145753ed6
Muhammet Ali KOÇ <71590181+malikoc2020@users.noreply.github.com>
2022-08-12 12:36:20 +0300

    Add project files.

---
+++ Domain/Base/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Base
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime DateCreated { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Domain/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Employee:BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }  
+        public string LastName { get; set; }
+    }
+}
